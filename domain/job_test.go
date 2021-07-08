86125b6466e11cad22f1075f6365aeb6baf4d260
wander4747 <wander.douglas14@gmail.com>
2021-07-08 18:59:49 -0300

    refator job entity and test

---
+++ domain/job_test.go
@@ -0,0 +1,36 @@
+package domain_test
+
+import (
+	"encoder/domain"
+	"testing"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewJob(t *testing.T) {
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	job, err := domain.NewJob("path", "Converted", video)
+
+	require.NotNil(t, job)
+	require.Nil(t, err)
+}
+
+func TestValidate(t *testing.T) {
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	job, _ := domain.NewJob("path", "Converted", video)
+	job.ID = "abc"
+
+	err := job.Validate()
+
+	require.Error(t, err)
+}
